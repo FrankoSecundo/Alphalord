f34fcd9c879749face647b47ec370429926b9622
FrankoSecundo <rm.jones652@gmail.com>
2025-01-16 14:18:29 -0500

    sending to the king

---
+++ running script.R
@@ -0,0 +1,15 @@
+###
+
+#running through other devtools commands in relation to Git
+
+# 16 Jan 2025
+# RMJ
+
+library(devtools)
+
+#telling the comp to use git and it will auto setup a repository
+use_git()
+
+#and now we actually will use gitHUB essentially we need to actually conect to the hub itself. Take the repository and rapture it
+
+use_github()
